4376518e524f03a828dc121ca99fa0b88dfdd5cf
Vaculik Josef (uig54189) <josef.vaculik@barum.cz>
2024-02-07 14:52:41 +0100

    IHttpActionResult, Mapper and Dto for Movies

---
+++ Vidly/Dtos/MovieDto.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using Vidly.Models;
+
+namespace Vidly.Dtos
+{
+    public class MovieDto
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public byte GenreId { get; set; }
+
+        [Required]
+        public DateTime ReleaseDate { get; set; }
+
+        [Required]
+        public DateTime DateAdded { get; set; }
+
+        [Required]
+        [Range(1, 20, ErrorMessage = "The field Number in Stock must be between 1 and 20")]
+        public byte? NumberInStock { get; set; }
+    }
+}
